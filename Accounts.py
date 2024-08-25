20aab9bf02f0ff286e52f16633f6d71a28b0cb12
Reis Hymer <ht3filmandphoto@gmail.com>
2024-08-25 13:02:59 -0700

    Update Accounts.py

--- Accounts.py
+++ Accounts.py
@@ -1,11 +1,11 @@
 class Account:
-    def __init__(self, balance, interest_rate):
+    def __init__(self, balance, apr):
         self.balance = balance
-        self.interest_rate = interest_rate
-        self.interest = 0  # Initialize interest to 0
-    
+        self.apr = apr
+
     def set_balance(self, balance):
         self.balance = balance
-    
+
     def set_interest(self, interest):
         self.interest = interest
+
